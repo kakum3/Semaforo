f2910abc3669f830e3cbcb3301d51727adf60287
kakum3 <kakum_3@hotmail.com>
2022-06-26 07:47:27 +0000

    first commit

---
+++ src/js/component/semaforo.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+
+
+
+//include images into your bundle
+import rigoImage from "../../img/rigo-baby.jpg";
+
+//create your first component
+const Semaforo = () => {
+	const [selectLight, setSelectLight] = useState(null);
+	return (
+		<>
+		<div className="soporte"></div>
+		<div className="semaforo">
+		<div
+			onClick={() => setSelectLight("red")}
+			className={"light" +(selectLight == "red" ? " seleccionado  redSel" : " red")}></div>
+			<div
+			onClick={() => setSelectLight("yellow")}
+			className={"light" +(selectLight == "yellow" ? " seleccionado yellowSel" : " yellow")}></div>
+			<div
+			onClick={() => setSelectLight("green")}
+			className={"light" +(selectLight == "green" ? " seleccionado greenSel" : " green")}></div>
+		</div>
+		</>
+	);
+};
+
+export default Semaforo;
